e7505197880330da62308e826d78222e1619282f
RushaliSoni <rushalilangaliya2@gmail.com>
2022-06-16 15:43:32 +0530

    Read and Write  Data From Json File Using Function

--- textExample.py
+++ textExample.py
@@ -1,5 +1,16 @@
-import json
-# read Data From Json File
-with open('text.json','r') as f:
-    s = f.read()
-    print(s)
+# import json
+# # read Data From Json File
+# with open('text.json','r') as f:
+#     s = f.read()
+#     print(s)
+
+# Using Function REad and Write Data From  Json File With Changes
+def find_Replace(find, replace, input, output):
+    with open(input) as f:
+        s = f.read()
+    s = s.replace(find, replace)
+    with open(output, "w") as f:
+        f.write(s)
+
+
+find_Replace("+type", "+size@5+type", "test.json", "output.json")
